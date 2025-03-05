3e27c7a0bdc5f78311c8896dbb75370e1cec0a35
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-03-05 15:12:22 +0100

    implement navigation with stack and add screens for Home, Add Excuse, and Excuse Details

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import { RootStackParamList } from "./navigation/types";
+import HomeScreen from "./screens/HomeScreen";
+import AddExcuseScreen from "./screens/AddExcuseScreen";
+import ExcuseDetailScreen from "./screens/ExcuseDetailScreen";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({navigation}) => ({
+            title: "Excuses",
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate("AddExcuse")}
+              >
+                <Icon name="plus" size={24} color="black"/>
+              </TouchableOpacity>
+
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="AddExcuse"
+          component={AddExcuseScreen}
+          options={{title: "Add Excuse"}}
+        />
+        <Stack.Screen
+          name="ExcuseDetail"
+          component={ExcuseDetailScreen}
+          options={{title: "Excuse Details"}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
