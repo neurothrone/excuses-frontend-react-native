953b3533c93ece6fde45ad9cf634bd592a6684c0
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-03-09 22:05:12 +0100

    set initial route for stack navigator to Home

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
           component={HomeScreen}
